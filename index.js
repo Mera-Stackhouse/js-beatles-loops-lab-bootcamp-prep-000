a991ee0ac96d1cbaa61b8d073d91c1d0d25a412a
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 06:51:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+function theBeatlesPlay(musiciansArray, instrumentsArray) {
+  //console.log(musiciansArray)
+  var newArray = []
+  for (var i = 0; i<musiciansArray.length; i++) {
+    var string = `${musiciansArray[i]} plays ${instrumentsArray[i]}.`
+    newArray[i] = string
+  }
+  console.log(newArray)
+  return newArray 
+}
+
+function johnLennonFacts(factsArray) {
+  var newArray = []
+  var i = 0
+  while (i < factsArray.lenth) {
+    var string = factsArray[i] + "!!!"
+    newArray[i] = string
+    i--
+  }
+  return newArray
+}
+
+function iLoveTheBeatles(number) {
+  var newArray = []
+  var i = number
+  do {
+    newArray[i] = 'I love the Beatles!'
+  } while (i < 15)
+  return newArray
+}
+
+iLoveTheBeatles(2)
