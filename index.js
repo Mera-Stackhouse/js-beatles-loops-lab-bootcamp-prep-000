db8ffbee1347ca3fe77cdbe91f0603e85bd34fc7
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 09:02:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts(factsArray) {
   var i = 0
   console.log(factsArray)
   console.log(factsArray.length)
-  while (i < 2) {
+  while (i < factsArray.length) {
     console.log(factsArray)
     newArray[i] = factsArray[i] + "!!!"
     i++
